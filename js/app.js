8e2fca08e3363a945f6eff310774308ed1c7727e
javier <javi_hen@hotmail.com>
2020-05-03 19:45:33 -0400

    modificacion de ventanas modales

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
-var seccionesPagina = new fullpage('#fullpage',{
-
-    
+var seccionesPagina = new fullpage("#fullpage", {
+  // ──────────────────────────────────────────────────
+  //   :::::: Opciones Basicas
+  // ──────────────────────────────────────────────────
+  autoScrolling: true, // Se activa el scroll.
+  fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
+  fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
+  easing: "easeInOutCubic", // Funcion de tiempo de la animacion.
+  scrollingSpeed: 700, // Velocidad del scroll. Valores: en milisegundos.
+  css3: true, // Si usara CSS3 o javascript.
+  easingcss3: "ease-out", // Curva de velocidad del efecto.
+  loopBottom: false, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
 });
